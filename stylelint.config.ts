305a65f60bee205020a5c77dee6c98327b1720ec
Mariana Kolinska <kolmary1818@gmail.com>
2026-08-24 23:46:24 +0300

    chore: update stylelint config to ignore tailwind theme kp-57

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -3,6 +3,12 @@ import { type Config } from "stylelint";
 const config: Config = {
 	extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
 	rules: {
+		"at-rule-no-unknown": [
+			true,
+			{
+				ignoreAtRules: ["theme"],
+			},
+		],
 		"color-hex-length": "long",
 		"declaration-no-important": true,
 		"max-nesting-depth": 0,
