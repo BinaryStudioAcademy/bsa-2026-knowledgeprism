945b98556305833d5bc88ca9aa4319c9d1a10856
Aqshin <aqshin.baghirzade@gmail.com>
2026-08-26 18:04:03 +0400

    feat: create common toggle component kp-56

--- apps/web/src/components/components.ts
+++ apps/web/src/components/components.ts
@@ -4,5 +4,6 @@ export { Input } from "./input/input.js";
 export { Link } from "./link/link.js";
 export { Select } from "./select/select.js";
 export { type SelectOption } from "./select/select.js";
+export { Toggle } from "./toggle/toggle.js";
 export { Provider as StoreProvider } from "react-redux";
 export { Outlet as RouterOutlet } from "react-router-dom";
