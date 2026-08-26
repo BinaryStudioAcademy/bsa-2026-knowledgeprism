7c3b5643b061133207db1002a18f5e31010bc9da
guja kupreishvili <gujakupreishvili4@gmail.com>
2026-08-26 16:17:06 +0400

    fix: scope footer styles
    Refs kp-51

--- apps/web/src/components/footer/footer.tsx
+++ apps/web/src/components/footer/footer.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
 		<footer>
 			<p>© 2026 KnowledgePrism AI. Order from Chaos.</p>
 			<ul>
+				{/* at this moment i left just li and when we have similar pages after this i will add routes */}
 				<li>Privacy</li>
 				<li>Terms</li>
 				<li>API Documentation</li>
