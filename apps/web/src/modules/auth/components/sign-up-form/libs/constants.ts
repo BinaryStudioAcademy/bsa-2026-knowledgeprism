17cabb816a742639fec0e2252b977d843b452cd6
Ivan Razumovskyi <ivanrazumovskyi@gmail.com>
2026-08-28 13:36:21 +0200

    feat: add auth register endpoint kp-2

--- apps/web/src/modules/auth/components/sign-up-form/libs/constants.ts
+++ apps/web/src/modules/auth/components/sign-up-form/libs/constants.ts
@@ -2,6 +2,9 @@ import { type UserSignUpRequestDto } from "@knowledgeprism/types";
 
 const DEFAULT_SIGN_UP_PAYLOAD: UserSignUpRequestDto = {
 	email: "",
+	firstName: "First name",
+	lastName: "Last name",
+	organisationName: "Organisation",
 	password: "",
 };
 
