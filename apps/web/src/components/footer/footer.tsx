fca52969475eb60762b99518ef8f9343a17a6edb
guja kupreishvili <gujakupreishvili4@gmail.com>
2026-08-28 13:35:11 +0400

    refactor: remove footer CSS file and update Footer component styles kp-51

--- apps/web/src/components/footer/footer.tsx
+++ apps/web/src/components/footer/footer.tsx
@@ -1,14 +1,18 @@
-import "./footer.css";
-
 function Footer() {
 	return (
-		<footer>
-			<p>© 2026 KnowledgePrism AI. Order from Chaos.</p>
-			<ul>
+		<footer className="flex items-center justify-between w-full px-[10%] bg-border">
+			<p className="text-text-faint font-mono font-normal text-sm">
+				© 2026 KnowledgePrism AI. Order from Chaos.
+			</p>
+			<ul className="flex items-center gap-6 py-5  ">
 				{/* at this moment i left just li and when we have similar pages after this i will add routes */}
-				<li>Privacy</li>
-				<li>Terms</li>
-				<li>API Documentation</li>
+				<li className="text-sm text-text-muted font-sans font-normal">
+					Privacy
+				</li>
+				<li className="text-sm text-text-muted font-sans font-normal">Terms</li>
+				<li className="text-sm text-text-muted font-sans font-normal">
+					API Documentation
+				</li>
 			</ul>
 		</footer>
 	);
