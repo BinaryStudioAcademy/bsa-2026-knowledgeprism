106ddfaf749adfed5f4240a31544b0349f98061e
“Roman <romanbilynskyy@gmail.com>
2026-08-29 17:53:28 +0300

    feat: create upload component kp-97

--- apps/web/src/lib/enums/enums.ts
+++ apps/web/src/lib/enums/enums.ts
@@ -1,5 +1,6 @@
 export { AppRoute } from "./app-route.enum.js";
 export { DataStatus } from "./data-status.enum.js";
+export { UploadFileStatus } from "./upload-file-status.enum.js";
 export {
 	APIPath,
 	AppEnvironment,
