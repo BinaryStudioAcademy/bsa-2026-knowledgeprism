74b68fec4ca7cda2da36090bf22fb4c91edc631c
Aqshin <aqshin.baghirzade@gmail.com>
2026-08-30 23:23:37 +0400

    feat: create common alert component kp-95

--- apps/web/src/components/components.ts
+++ apps/web/src/components/components.ts
@@ -1,3 +1,4 @@
+export { Alert } from "./alert/alert.js";
 export { Button } from "./button/button.js";
 export { Checkbox } from "./checkbox/checkbox.js";
 export { Input } from "./input/input.js";
