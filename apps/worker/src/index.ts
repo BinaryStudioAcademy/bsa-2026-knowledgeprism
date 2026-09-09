fc393bf24a03c2f42cfe42a947722a097b703e48
Mayar <mayarkalmakahleh@gmail.com>
2026-09-09 16:03:31 +0300

    feat: add knowledge item type kp-159

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,2 +1,3 @@
 export { bedrockRuntimeClient } from "./bedrock/bedrock.js";
 export { logger } from "./logger/logger.js";
+export { type KnowledgeItem } from "./modules/knowledge-extraction/libs/types/types.js";
