c10e9a1164296c32a15c7b8e43ce3f8f1ddfd25a
Mayar <mayarkalmakahleh@gmail.com>
2026-09-11 05:54:30 +0300

    feat: add extraction output helpers kp-159

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,4 +1,6 @@
 export { bedrockRuntimeClient } from "./bedrock/bedrock.js";
 export { logger } from "./logger/logger.js";
 export { ClaudeModelId } from "./modules/knowledge-extraction/libs/constants/claude-model.constant.js";
+export { isBlankPageContent } from "./modules/knowledge-extraction/libs/helpers/is-blank-page-content.helper.js";
+export { mapExtractionOutput } from "./modules/knowledge-extraction/libs/helpers/map-extraction-output.helper.js";
 export { type KnowledgeItem } from "./modules/knowledge-extraction/libs/types/types.js";
