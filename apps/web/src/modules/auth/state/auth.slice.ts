355112882ed3571b82ff567c586dc5581414fcf0
OvasProg <alex.vasyliev.prog@gmail.com>
2026-09-14 12:58:12 +0300

    feat: add user management api client and redux state kp-120

--- apps/web/src/modules/auth/state/auth.slice.ts
+++ apps/web/src/modules/auth/state/auth.slice.ts
@@ -24,9 +24,10 @@ const { actions, name, reducer } = createSlice({
 			state.dataStatus = DataStatus.PENDING;
 			state.error = null;
 		});
-		builder.addCase(signIn.fulfilled, (state) => {
+		builder.addCase(signIn.fulfilled, (state, action) => {
 			state.dataStatus = DataStatus.FULFILLED;
 			state.error = null;
+			state.user = action.payload;
 		});
 		builder.addCase(signIn.rejected, (state, action) => {
 			state.dataStatus = DataStatus.REJECTED;
