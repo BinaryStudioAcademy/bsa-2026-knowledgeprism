da57f7efb402c4fa1a2f9806d0022b9661ae6468
Sofia Matthaiaki <93294647+Sophie-Mat@users.noreply.github.com>
2026-09-15 19:46:03 +0300

    fix: scroll to top on landing logo click kp-226

--- apps/web/src/modules/landing/components/landing-header/libs/constants.ts
+++ apps/web/src/modules/landing/components/landing-header/libs/constants.ts
@@ -18,6 +18,8 @@ const HEADER_SECTION_LINKS = [
 	{ href: "#features", label: "Features" },
 ] as const;
 
+const SCROLL_TO_TOP_POSITION = 0;
+
 export {
 	HEADER_LABEL,
 	HEADER_MENU_ICON_HEIGHT,
@@ -25,4 +27,5 @@ export {
 	HEADER_MENU_ICON_WIDTH,
 	HEADER_NAV_ID,
 	HEADER_SECTION_LINKS,
+	SCROLL_TO_TOP_POSITION,
 };
