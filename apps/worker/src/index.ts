09c70183c2da597f976e51d27e33a259e0552eee
Mayar <mayarkalmakahleh@gmail.com>
2026-09-15 14:06:50 +0300

    feat: add document parse types and errors kp-229

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,1 +1,4 @@
-export {};
+export { DocumentContentType } from "./parsers/libs/enums/document-content-type.enum.js";
+export { DocumentParseFailedError } from "./parsers/libs/exceptions/document-parse-failed.exception.js";
+export { UnsupportedDocumentFormatError } from "./parsers/libs/exceptions/unsupported-document-format.exception.js";
+export { type ParsedPageBlock } from "./parsers/libs/types/parsed-page-block.type.js";
