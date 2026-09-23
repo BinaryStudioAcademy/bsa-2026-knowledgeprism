706d2943709bb2437308b7b7e0559317f06b3c7a
Aqshin <aqshin.baghirzade@gmail.com>
2026-09-23 13:13:23 +0400

    fix: align manual text project authorization kp-294

--- apps/api/src/modules/documents/documents.ts
+++ apps/api/src/modules/documents/documents.ts
@@ -6,16 +6,13 @@ import { projectService } from "~/modules/projects/projects.js";
 import { DocumentController } from "./controllers/document.controller.js";
 import { DocumentModel } from "./models/document.model.js";
 import { DocumentRepository } from "./repositories/document.repository.js";
-import { DocumentAccessService } from "./services/document-access.service.js";
 import { DocumentProcessor } from "./services/document-processor.js";
 import { DocumentService } from "./services/document.service.js";
 
 const documentRepository = new DocumentRepository(DocumentModel);
-const documentAccessService = new DocumentAccessService();
 const documentProcessor = new DocumentProcessor();
 const documentService = new DocumentService({
 	checkDocumentObjectExists,
-	documentAccessService,
 	documentProcessor,
 	documentRepository,
 	generatePresignedUploadUrl,
